08297635856dd589d1cb78f997911920d8fa47e7
Vynax <22928806+Vynax@users.noreply.github.com>
2021-11-12 23:44:22 +0800

    成功讀取出 HW2 的座標並繪製出分割線

--- character.js
+++ character.js
@@ -13,13 +13,14 @@ export class Character {
         this.right_horizon_edges = [];
         this.right_vertical_edges = [];
 
+        //左右各5條橫線
         for (let i = 0; i < this.horizon_line; i++) {
             this.left_horizon_edges[i] = new Edge(color_hex[0]);
             this.right_horizon_edges[i] = new Edge(color_hex[2]);
             scene.add(this.left_horizon_edges[i].line);
             scene.add(this.right_horizon_edges[i].line);
         }
-
+        //左右各9條直線
         for (let i = 0; i < this.vertical_line; i++) {
             this.left_vertical_edges[i] = new Edge(color_hex[1]);
             this.right_vertical_edges[i] = new Edge(color_hex[3]);
@@ -31,6 +32,9 @@ export class Character {
         this.x2 = 0;
         this.y1 = 0;
         this.y2 = 0;
+
+        this.x_offset = 0;
+        this.y_offset = 0;
         // this.mesh = 0;
         this.file_path = file_path;
     }
@@ -41,10 +45,42 @@ export class Character {
         this.y1 = json_data['line_locationy_1'];
         this.y2 = json_data['line_locationy_2'];
 
-        console.log("x1" + this.x1);
-        console.log("x2" + this.x2);
-        console.log("y1" + this.y1);
-        console.log("y1" + this.y2);
+        console.log("x1:" + this.x1);
+        console.log("x2:" + this.x2);
+        console.log("y1:" + this.y1);
+        console.log("y2:" + this.y2);
+
+        //每條線的偏移量
+        this.x_offset = Math.abs(this.x2 - this.x1) / 8;
+        this.y_offset = Math.abs(this.y2 - this.y1) / 4;
+
+        let temp_Vector3 = [];
+        temp_Vector3[0] = new THREE.Vector3();
+        temp_Vector3[1] = new THREE.Vector3();
+
+        //左右各5橫線
+        for (let i = 0; i < this.horizon_line; i++) {
+            let y_temp = this.y1 + i * this.y_offset;
+            temp_Vector3[0].set(0, y_temp, 20);
+            temp_Vector3[1].set(10, y_temp, 20);
+            this.left_horizon_edges[i].set_Line_Points(temp_Vector3[0], temp_Vector3[1]);
+            temp_Vector3[0].set(0, y_temp, 20);
+            temp_Vector3[1].set(-10, y_temp, 20);
+            this.right_horizon_edges[i].set_Line_Points(temp_Vector3[0], temp_Vector3[1]);
+        }
+        let y_mid = (this.y1 + this.y2) / 2;
+
+        //左右各9條直線
+        for (let i = 0; i < this.vertical_line; i++) {
+            let x_temp = this.x1 + i * this.x_offset;
+            temp_Vector3[0].set(x_temp, y_mid - 5, 20);
+            temp_Vector3[1].set(x_temp, y_mid + 5, 20);
+            this.left_vertical_edges[i].set_Line_Points(temp_Vector3[0], temp_Vector3[1]);
+            temp_Vector3[0].set(-x_temp, y_mid - 5, 20);
+            temp_Vector3[1].set(-x_temp, y_mid + 5, 20);
+            this.right_vertical_edges[i].set_Line_Points(temp_Vector3[0], temp_Vector3[1]);
+        }
+
     }
 
     set_Line_Points(edge_now, point1, point2) {
@@ -68,8 +104,19 @@ export class Character {
     }
 
     set_Visible(true_or_false) {
-        for (let i = 0; i < this.eye_edges.length; i++) {
+        /* for (let i = 0; i < this.eye_edges.length; i++) {
             this.eye_edges[i].line.visible = true_or_false;
+        } */
+
+        //左右各5條橫線
+        for (let i = 0; i < this.horizon_line; i++) {
+            this.left_horizon_edges[i].line.visible = true_or_false;
+            this.right_horizon_edges[i].line.visible = true_or_false;
+        }
+        //左右各9條直線
+        for (let i = 0; i < this.vertical_line; i++) {
+            this.left_vertical_edges[i].line.visible = true_or_false;
+            this.right_vertical_edges[i].line.visible = true_or_false;
         }
         this.mesh.visible = true_or_false;
     }
